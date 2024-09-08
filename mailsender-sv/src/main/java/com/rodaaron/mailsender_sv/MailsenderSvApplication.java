6e0635e6c5c5ffd9cb876942c993bb7937aa47c4
Roman Lepore <romanlepore30@gmail.com>
2024-09-08 15:13:56 -0300

    add mail sender service to the proyect

---
+++ mailsender-sv/src/main/java/com/rodaaron/mailsender_sv/MailsenderSvApplication.java
@@ -0,0 +1,13 @@
+package com.rodaaron.mailsender_sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailsenderSvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailsenderSvApplication.class, args);
+	}
+
+}
---
+++ mailsender-sv/src/test/java/com/rodaaron/mailsender_sv/MailsenderSvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodaaron.mailsender_sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailsenderSvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
